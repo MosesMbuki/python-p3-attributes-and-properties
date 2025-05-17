e1c696e8bd2d150019a2b1382a492381b772fea3
+ <moses.mutitu@student.moringaschool.com>
2025-05-17 16:28:29 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,43 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+APPROVED_BREEDS = [
+    "Mastiff",
+    "Chihuahua", 
+    "Corgi",
+    "Shar Pei",
+    "Beagle",
+    "French Bulldog",
+    "Pug",
+    "Pointer"
+]
+
 class Dog:
+    def __init__(self, name="", breed="Mutt"):
+        self._name = None
+        self._breed = None
+        self.name = name  # This triggers the name setter
+        self.breed = breed  # This triggers the breed setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS or value == "Mutt":
+            self._breed = value
+        else:
+             print("Breed must be in list of approved breeds.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,38 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self, name="", job=""):
+        self._name = None
+        self._job = None
+        self.name = name  
+        self.job = job    
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()  # Convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:  
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
     pass
+
+person = Person()
+person.name = "john doe"
+person.job = "Dictator"
+print(person.name)  # Output: John Doe
+print(person.job)   # Output: Dictator
